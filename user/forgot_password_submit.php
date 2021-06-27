0346d1d39cbcdeb0039ea877eb7fe954275b227a
nishajega <73438376+nishajega@users.noreply.github.com>
2021-06-27 21:46:34 +0800

    Update forgot_password_submit.php

--- user/forgot_password_submit.php
+++ user/forgot_password_submit.php
@@ -19,9 +19,9 @@ if($check_user>0){
 	$mail->CharSet ='UTF-8';
 	$mail->SMTPSecure="tls";
 	$mail->SMTPAuth=true;
-	$mail->Username="vesha2797@gmail.com";
-	$mail->Password="Veshadoll2712";
-	$mail->SetFrom("vesha2797@gmail.com");
+	$mail->Username="yourEmail";
+	$mail->Password="emailPassword";
+	$mail->SetFrom("email");
 	$mail->addAddress($email);
 	$mail->IsHTML(true);
 	$mail->Subject="Your Password";
